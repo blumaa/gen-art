dd984a5054d0b0c3ceb581d074012d5a69283216
Aaron Blum <blumaa@gmail.com>
2023-01-08 16:29:54 +0100

    feat: advanced gen-art

--- sketch.js
+++ sketch.js
@@ -5,12 +5,14 @@ const palettes = require('nice-color-palettes')
 
 const settings = {
   suffix: random.getSeed(),
-  // dimensions: [2048, 2048], // in cm according to units
+  dimensions: [2048, 2048], // in cm according to units
+  pixelsPerInch: 300,
   // units: "cm",
-  dimensions: "A4"
+  // dimensions: "A4",
 };
 
-random.setSeed(random.getRandomSeed())
+random.setSeed(151352)
+// random.setSeed(random.getRandomSeed())
 console.log(random.getSeed())
 
 const sketch = () => {
@@ -19,27 +21,24 @@ const sketch = () => {
 
   const createGrid = () => {
     const points = [];
-    const count = 50;
+    const count = 400;
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
         const position = [u, v];
-        const radius = Math.abs(random.noise2D(u, v) * 0.015);
-        const radius2 = Math.abs(random.noise2D(u, v) * 0.35);
+        const radius = Math.abs(random.noise2D(u, v) * 0.05);
         const symbol = '\u{2425}';
         const deadMan = '\u{1303F}';
         const leftFacingSvasti = '\u{0FD6}'
         const omega = '\u{03A9}';
         points.push({
           color: random.pick(palette),
-          color2: random.pick(palette),
           // radius: Math.max(0, random.gaussian() * 0.008),
           rotation: random.noise2D(u, v),
           radius,
-          radius2,
-          symbol: symbol,
+          symbol: leftFacingSvasti,
           position
         })
       }
@@ -49,22 +48,20 @@ const sketch = () => {
 
   // random.setSeed(512)
   const points = createGrid().filter(() => random.value() > 0.8);
-  const points2 = createGrid().filter(() => random.value() > 0.2);
 
   return ({ context, width, height }) => {
     // const margin = width * 0.175;
-    const margin = 100;
+    const margin = 300;
 
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height)
 
-    points2.forEach(data => {
+
+    points.forEach(data => {
       const {
         color,
-        color2,
         position,
         radius,
-        radius2,
         symbol,
         rotation
       } = data;
@@ -75,37 +72,21 @@ const sketch = () => {
 
       context.save()
       context.fillStyle = color;
-      context.font = `${radius2 * width}px "Arial"`
+      context.font = `${radius * width}px "Roboto"`
       context.translate(x, y);
       context.rotate(rotation);
       context.fillText(symbol, 0, 0);
       context.restore();
-    });
-
-    points.forEach(data => {
-      const {
-        color,
-        color2,
-        position,
-        radius,
-        symbol,
-        rotation
-      } = data;
-
-      const [u, v] = position;
-      const x = lerp(margin, width - margin, u);
-      const y = lerp(margin, height - margin, v);
-
-      context.beginPath();
+      // context.beginPath();
       // arc(x, y, radius, startAngle, endAngle, counterclockwise)
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
       // context.rect(x, y, width / 15, height / 15);
       // context.fillText("hello", x, y)
       // context.ellipse(x, y, radius * width, radius * height, 0, Math.PI * 2, false);
-      context.strokeStyle = color2;
-      context.lineWidth = 30;
-      context.fillStyle = color2;
-      context.stroke();
+      // context.strokeStyle = color;
+      // context.lineWidth = 1;
+      // context.fillStyle = color;
+      // context.stroke();
     });
   };
 };
